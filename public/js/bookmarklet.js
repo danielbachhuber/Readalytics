4f88e859b624faa4f88368af34012ef4eae20c60
Daniel Bachhuber <d@danielbachhuber.com>
2011-04-23 16:11:54 -0400

    More work on the bookmarklet

--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -5,12 +5,12 @@ function readalytics_bookmarklet() {
 	w = window,
 	l = d.location,
 	z = d.createElement('scr' + 'ipt'),
-	s = 'http://127.0.0.1:3000/bookmarklet.js';
+	s = 'http://127.0.0.1:3000/bookmarklet/load/';
 	
 	try {
         if (!b) throw (0);
         d.title = '(Analyzing...) ' + d.title;
-        z.setAttribute('src', s + '?url=' + encodeURIComponent(l.href) + (new Date().getTime()));
+        z.setAttribute('src', s + '' + encodeURIComponent(l.href));
         b.appendChild(z);
     } catch(e) {
         alert('Please wait until the page has loaded.');
