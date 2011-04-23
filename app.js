2bb5decde208ff8a7d5f0b29c458f499b2bdf4f9
Daniel Bachhuber <d@danielbachhuber.com>
2011-04-23 16:11:05 -0400

    Pass the URL as a param

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ ejs.close = '}}';
 
 console.log('Node started');
 
-app.get('/bookmarklet.js', function(req, res) {
+app.get('/bookmarklet/load/:url', function(req, res) {
 	res.contentType('text/javascript');
- 	res.render('bookmarklet.ejs', { layout: false });
+ 	res.render( 'bookmarklet.ejs', { layout: false, url: req.params.url });
 });
 
 app.get('/', function(req, res) {
