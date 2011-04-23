42e6565fdd1aa4f037cdf56c45cc0193e14bb3fa
Daniel Bachhuber <d@danielbachhuber.com>
2011-04-23 15:19:54 -0400

    We have a bookmarklet that executes

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var app = require('express').createServer();
-var jade = require('jade');
-
 app.set('view engine', 'jade');
 app.set('views', __dirname+'/views');
 
+var jade = require('jade');
+
+var ejs = require('ejs');
+ejs.open = '{{';
+ejs.close = '}}';
+
 console.log('Node started');
 
-app.get('/bookmarklet', function(req, res) {
- 	res.render('bookmarklet.jade', { layout: false });
+app.get('/bookmarklet.js', function(req, res) {
+	res.contentType('text/javascript');
+ 	res.render('bookmarklet.ejs', { layout: false });
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello world');
 });
 
 app.listen(3000);
--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -1,10 +1,11 @@
-function readalytics() {
+javascript:
+function readalytics_bookmarklet() {
 	var d = document,
 	b = d.body,
 	w = window,
 	l = d.location,
 	z = d.createElement('scr' + 'ipt'),
-	s = 'http://127.0.0.1:3000/bookmarklet/';
+	s = 'http://127.0.0.1:3000/bookmarklet.js';
 	
 	try {
         if (!b) throw (0);
@@ -15,5 +16,5 @@ function readalytics() {
         alert('Please wait until the page has loaded.');
     }
 }
-
+readalytics_bookmarklet();
 void(0);
